89450eef17f0860e51b1a99fd35002d08d39d3c6
Popov Evgenii <ev.popov@elma-bs.ru>
2024-03-26 14:29:16 +0600

    lesson-75

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import productsRouter from './routes/products';
 import fileDb from './fileDb';
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/products', productsRouter);
 
